341705785bd1782436c4cc768f6e11d152b93f9f
samirRamos039 <rzagza039@gmail.com>
2021-01-24 18:23:48 -0500

    clase modelo

---
+++ src/modelo/Modelo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author rzagza
+ */
+public class Modelo {
+    String numero;
+    int id;
+
+    public Modelo(String numero, int id) {
+        this.numero = numero;
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
